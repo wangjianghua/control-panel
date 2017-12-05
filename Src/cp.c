519f264517af5993635a8434e5aa72f1d39d6eec
wangjianghua <jianghua.wang@foxmail.com>
2017-12-05 15:32:42 +0800

    改动：上传完成后，通信正常 日期：2017.12.05
    Signed-off-by: wangjianghua <jianghua.wang@foxmail.com>

--- Src/cp.c
+++ Src/cp.c
@@ -3776,6 +3776,174 @@ void copy_upload_rate_init(void)
     }
 }
 
+CODE u8 copy_comm_reset_cmd[][32] = {
+    /* SET_CMD */
+	{0xF7, 0x17, 0x00, 0x59, 0x00, 0x0B, 0x00, 0x59, 0x00, 0x09, 0x12, 0x04, 0xA1, 0x50, 0x88, 0x00 ,0x04, 00, 00, 00, 0x08, 00, 00, 0x09, 0xC4, 00, 00, 00, 00},
+    {0xF7, 0x17, 0x00, 0x59, 0x00, 0x0C, 0x00, 0x59, 0x00, 0x02, 0x04, 0x12, 0xA1, 0x50, 0x15},
+    {0xF7, 0x17, 0x00, 0x59, 0x00, 0x03, 0x00, 0x59, 0x00, 0x03, 0x06, 0x06, 0xA2, 0x50, 0x82, 0x10, 0x01},
+};
+
+void copy_comm_reset(void)
+{
+    u8 i, len, timeout;
+    unsigned int crc;
+    
+    
+    for(i = 0; i < 3; i++)
+    {        
+        len = copy_comm_reset_cmd[i][10] + 11;
+                        
+        memcpy(UART_TX_BUF, copy_comm_reset_cmd[i], len);
+
+        switch(i)
+        {
+        case 0:
+            UART_TX_BUF[13] = (UART_TX_BUF[13] & 0xf0) | (g_cp_para.cmd & 0x0f);
+
+            if((4 == (UART_TX_BUF[11] & 0x0f)) && ((0xa1 == (UART_TX_BUF[12] & 0xff))))
+            {
+                UART_TX_BUF[15] = (u8)(g_cp_para.count >> 8);
+                UART_TX_BUF[16] = (u8)(g_cp_para.count & 0xff);
+
+                if(TRUE == g_cp_para.reset)
+                {
+                    g_cp_para.reset = FALSE;
+                    
+                    UART_TX_BUF[20] |= 0x10;
+                }
+
+                if(TRUE == g_cp_para.ref_chang)
+                {
+                    g_cp_para.ref_chang = FALSE;
+                    
+                    UART_TX_BUF[23] = (u8)(g_cp_para.ref_temp >> 8);
+                    UART_TX_BUF[24] = (u8)(g_cp_para.ref_temp >> 0);
+                }
+                else
+                {
+                    UART_TX_BUF[23] = (u8)(g_cp_para.ref >> 8);
+                    UART_TX_BUF[24] = (u8)(g_cp_para.ref >> 0);
+                }
+
+                if(TRUE == g_cp_para.stop)
+                {
+                    g_cp_para.stop = FALSE;
+                    
+                    UART_TX_BUF[20] |= 0x01;
+                }
+                
+                if(TRUE == g_cp_para.run)
+                {
+                    g_cp_para.run = FALSE;
+                    
+                    UART_TX_BUF[20] |= 0x02;
+                }
+
+                if(VFD_REV == g_cp_para.fr)
+                {
+                    UART_TX_BUF[20] |= 0x04;
+                }
+                else
+                {
+                    UART_TX_BUF[20] &= ~0x04;
+                }
+                
+                if(VFD_LOC == g_cp_para.lr)
+                {
+                    UART_TX_BUF[20] |= 0x08;
+                }
+                else
+                {
+                    UART_TX_BUF[20] &= ~0x08;
+                }
+            }
+            break;
+
+        case 1:
+            UART_TX_BUF[13] = (UART_TX_BUF[13] & 0xf0) | (g_cp_para.cmd & 0x0f);
+            break;
+
+        case 2:
+            UART_TX_BUF[13] = (UART_TX_BUF[13] & 0xf0) | (g_cp_para.cmd & 0x0f);
+            break;
+
+        default:
+            break;
+        }
+
+        crc = CRC16Calculate(UART_TX_BUF, len);
+        UART_TX_BUF[len++] = (u8)(crc & 0xff);
+        UART_TX_BUF[len++] = (u8)((crc & 0xff00) >> 8);
+        
+        uart_send(len);
+
+        /* CPTask与KeyTask已经有信号在通信，受限于RTX-51 TINY弱小的功能，
+         * 这里CPTask不能使用同一信号与UartTask进行通信，否则可能产生冲突，导致丢失信号
+         * 华兄 */
+        for(timeout = 0; timeout <= VFD_REPLY_TIMEOUT; timeout++) //等待变频器应答
+        {
+            /* 2500 = 1s */
+            os_wait(K_TMO, 25, 0);
+
+            if(TRUE == uart_rx_complete) //串口接收数据完毕
+            {
+                break;
+            }
+        }
+        
+        if(TRUE == uart_rx_complete)
+        {
+            uart_recv_align();
+            
+            if(0 == CRC16Calculate(UART_RX_BUF, uart_rx_count))
+            {
+                switch(i)
+                {
+                case 0:
+                    if((0x44 == UART_RX_BUF[3]) && (0xa1 == UART_RX_BUF[4]))
+                    {
+                        g_cp_para.count = ((u16)UART_RX_BUF[7] << 8) | ((u16)UART_RX_BUF[8]);
+                        g_cp_para.count++;
+
+                        if(UART_RX_BUF[11] & 0x80)
+                        {
+                            g_cp_para.reset = TRUE;
+                        }
+
+                        g_cp_para.ref = ((u16)UART_RX_BUF[15] << 8) | ((u16)UART_RX_BUF[16]);
+
+                        i = 2; //通信正常，跳过复位
+                    }
+                    break;
+
+                default:
+                    break;
+                }             
+            }
+            else
+            {
+                led_disp_buf[4] = 0xff;
+                led_disp_buf[3] = led_table['E' - 32];
+                led_disp_buf[2] = led_table['r' - 32];
+                led_disp_buf[1] = led_table['r' - 32];
+                led_disp_buf[0] = led_table[i + 16];
+                LEDOE = 0;
+            }
+        }
+        else
+        {
+            led_disp_buf[4] = 0xff;
+            led_disp_buf[3] = led_table['E' - 32];
+            led_disp_buf[2] = led_table['r' - 32];
+            led_disp_buf[1] = led_table['r' - 32];
+            led_disp_buf[0] = led_table[i + 16];
+            LEDOE = 0;
+        }
+
+        uart_recv_clear();
+    }
+}
+
 CODE u8 copy_upload_rate_cmd[][32] = {
     /* COPY_UPLOAD_RATE_SET_CMD */
 	{0xF7, 0x17, 0x00, 0x59, 0x00, 0x0B, 0x00, 0x59, 0x00, 0x09, 0x12, 0x04, 0xA1, 0x50, 0x88, 0x00 ,0x04, 00, 00, 00, 0x08, 00, 00, 0x09, 0xC4, 00, 00, 00, 00},
@@ -3957,6 +4125,8 @@ void form_copy_upload_rate_callback(void)
                         
                         if(TRUE == chang_baudrate(OTHER_BAUDRATE))
                         {
+                            copy_comm_reset();
+                            
                             form_id = FORM_ID_COPY_UPLOAD;
                         }
                     }
